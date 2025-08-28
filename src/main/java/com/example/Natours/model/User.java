582b771ed56d4bdd0f2464a8f318a0630b269a54
Krishnagokul1305 <krishnagokul1729@gmail.com>
2025-08-28 22:01:52 +0530

    added users folder structure

---
+++ src/main/java/com/example/Natours/model/User.java
@@ -0,0 +1,50 @@
+package com.example.Natours.model;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    @Indexed(unique = true)
+    private String email;
+
+    private String password;
+
+    private String photo = "default.jpg";
+
+    private LocalDateTime updatedAt;
+
+    private Role role = Role.USER;
+
+    private String passwordResetToken;
+
+    private LocalDateTime tokenExpireTime;
+
+    private Boolean active = true;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public enum Role {
+        USER,
+        ADMIN,
+        GUIDE,
+        LEAD_GUIDE
+    }
+
+    public boolean isUpdated(Long tokenIssuedAt) {
+        return this.updatedAt != null &&
+                tokenIssuedAt < this.updatedAt.toEpochSecond(java.time.ZoneOffset.UTC);
+    }
+}
